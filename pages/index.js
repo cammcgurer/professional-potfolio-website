ccbf3ee4d3821ce9011d5bed68759a08085f1d1c
Cameron McGurer <71100101+cammcgurer@users.noreply.github.com>
2024-05-07 18:37:30 -0400

    Update index.js,changed image path again

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
           <p> "short description because page not updating"</p> 
           <div>
             <Image
-              src="../assets/cam_headshots.png"
+              src="/cam_headshots.png"
               width={500}
               height={500}
             />
