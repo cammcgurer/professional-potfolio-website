0e3f952b5c0906bb96995cf7e7b3ce1ad423cfe3
Cameron McGurer <71100101+cammcgurer@users.noreply.github.com>
2024-05-07 18:03:15 -0400

    Update index.js, added placeholder sections

--- pages/index.js
+++ pages/index.js
@@ -21,9 +21,18 @@ export default function Home() {
             />
           </div>
         <Header title="About Me" />
-          <p>"As a recent graduate from The University of Rhode Island with honors, \
-              I am a young, energetic and determined individual who is invested in both business and technology. \
-              I want to be able to meld my tehnical experience and people skills together to make meaningful contributions to the people around me and the company I work for. 
+          <p>"I am a young, energetic and determined individual who is invested in business, technology, and the intersection between the two. \
+              I want to be able to meld my tehnical experience and people skills together to make meaningful contributions \
+              to the people around me and the company I work for."
+          </p>
+        <Header title="Projects" />
+          <p>"Projects go here" 
+          </p>
+        <Header title="Resume" />
+          <p>"pdf of resume" 
+          </p>
+        <Header title="Contact Me" />
+          <p>"Contact info (github, linkedin, email)" 
           </p>
       </main>
 
