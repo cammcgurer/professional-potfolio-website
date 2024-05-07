71dc179f2ea2fca3bac1354c2b986682702b5a0d
Cameron McGurer <71100101+cammcgurer@users.noreply.github.com>
2024-05-07 17:56:49 -0400

    Update index.js, profile picture, and about me

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,19 @@ export default function Home() {
 
       <main>
         <Header title="Hello, I'm Cameron" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+          <div>
+            <Image
+              src="../assets/cam_headshots.png"
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
+          </div>
+        <Header title="About Me" />
+          <p>"As a recent graduate from The University of Rhode Island with honors, \
+              I am a young, energetic and determined individual who is invested in both business and technology. \
+              I want to be able to meld my tehnical experience and people skills together to make meaningful contributions to the people around me and the company I work for. 
+          </p>
       </main>
 
       <Footer />
