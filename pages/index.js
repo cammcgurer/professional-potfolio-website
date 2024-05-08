723d1b9d543a2a7c250334e1ddee2d06de017414
Cameron McGurer <71100101+cammcgurer@users.noreply.github.com>
2024-05-07 20:43:18 -0400

    Update index.js, added intro-scetion class

--- pages/index.js
+++ pages/index.js
@@ -6,40 +6,40 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className="container">
-      // <Head>
-      //   <title>Next.js Starter!</title>
-      //   <link rel="icon" href="/favicon.ico" />
-      // </Head>
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
       <main>
-        <div>
-            <h1> Hello, I'm Cameron </h1>
-            <p> I am a young, energetic and determined individual who is invested in business, 
-                technology, and the intersection between the two. I want to be able to meld my tehnical experience and people skills together 
-                to make meaningful contributions to the people around me and the company I work for.
+        <div className="intro-section">  {/* Add a new div with class "intro-section" */}
+          <div className="image-container">  {/* Container for the image */}
+            <Image
+              src="/cam_headshot.png"
+              width={325}
+              height={325}
+              alt="Cameron"
+              style={{ borderRadius: '50%' }}
+            />
+          </div>
+          <div className="intro-text">  {/* Container for the text */}
+            <h1>Hello, I'm Cameron</h1>
+            <p>I am a young, energetic and determined individual who is invested in business,
+              technology, and the intersection between the two. I want to be able to meld my technical experience and people skills together
+              to make meaningful contributions to the people around me and the company I work for.
             </p>
-        </div>
-        <div>
-          <Image
-            src="/cam_headshot.png"
-            width={325}
-            height={325}
-            alt="Cameron"
-            style={{ borderRadius: '50%' }}
-          />
+          </div>
         </div>
         <Header title="Projects" />
-          <p>"Projects go here" 
-          </p>
+        <p>Projects go here</p>
         <Header title="Resume" />
-          <p>"pdf of resume" 
-          </p>
+        <p>pdf of resume</p>
         <Header title="Contact Me" />
-          <p>"Contact info (github, linkedin, email)" 
-          </p>
+        <p>Contact info (github, linkedin, email)</p>
       </main>
 
       <Footer />
     </div>
   )
 }
+
