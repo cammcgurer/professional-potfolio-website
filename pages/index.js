07ddeb6f5332d7c7db5a4ffb47274dcddca1b9e2
Cameron McGurer <71100101+cammcgurer@users.noreply.github.com>
2024-05-07 23:18:59 -0400

    Update index.js, placeholder for project list

--- pages/index.js
+++ pages/index.js
@@ -45,8 +45,22 @@ export default function Home() {
           </div>
         </div>
 
+        <Header title="Projects" />
         <div className="project-section">
-          <Header title="Projects" />
+          <ul className="projects-list">
+            <li>
+              <h3>Project Title 1</h3>
+              <p>Description of Project 1.</p>
+            </li>
+            <li>
+              <h3>Project Title 2</h3>
+              <p>Description of Project 2.</p>
+            </li>
+            <li>
+              <h3>Project Title 3</h3>
+              <p>Description of Project 3.</p>
+            </li>
+          </ul>
         </div>
 
         <Header title="Resume" />
