cfde5770b755bc8f42809867b219ced376d6c7b5
Cameron McGurer <71100101+cammcgurer@users.noreply.github.com>
2024-05-07 22:17:39 -0400

    Update index.js, added resume

--- pages/index.js
+++ pages/index.js
@@ -56,6 +56,7 @@ export default function Home() {
 
         <div className="resume-section">
           <Header title="Resume" />
+          <embed src="Cam_McGurer_Resume.pdf" width="800px" height="2100px" />
         </div>
       </main>
 
