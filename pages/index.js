3a7486b8a668153390cb49eb4b21e6e40c5ada3b
Cameron McGurer <71100101+cammcgurer@users.noreply.github.com>
2024-05-07 20:18:19 -0400

    Update index.js, changed picture

--- pages/index.js
+++ pages/index.js
@@ -12,18 +12,23 @@ export default function Home() {
       </Head>
 
       <main style={{ display: 'flex', alignItems: 'center', gap: '20px' }}>
-        <Header title="Hello, I'm Cameron" />
         <div>
           <Image
             src="/cam_headshot.png"
-            width={500}
-            height={500}
+            width={325}
+            height={325}
             alt="Cameron"
             style={{ borderRadius: '50%' }}
           />
         </div>
-        <Header title="About Me" />
-          <p>I am a young, energetic and determined individual who is invested in business, technology, and the intersection between the two. I want to be able to meld my tehnical experience and people skills together to make meaningful contributions to the people around me and the company I work for.</p>
+        <div>
+            <h1> Hello, I'm Cameron </h1>
+            <p> I am a young, energetic and determined individual who is invested in business, 
+                technology, and the intersection between the two. I want to be able to meld my tehnical experience and people skills together 
+                to make meaningful contributions to the people around me and the company I work for.
+            </p>
+          />
+        </div>
         <Header title="Projects" />
           <p>"Projects go here" 
           </p>
