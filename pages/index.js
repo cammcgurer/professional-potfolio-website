5d6e72c266f908b997e34b939890b67e1bc06d88
Cameron McGurer <71100101+cammcgurer@users.noreply.github.com>
2024-05-11 12:22:53 -0400

    Update index.js, changed page title

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Cameron McGurer Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
