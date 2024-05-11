06f4a023195e151203d51bc3ce63c715cf91a3d1
Cameron McGurer <71100101+cammcgurer@users.noreply.github.com>
2024-05-11 12:16:02 -0400

    Update next.config.js, removed previous changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",  // <=== enables static exports
-  basePath: "/professional-potfolio-website"
   reactStrictMode: true,
   swcMinify: true
 };
