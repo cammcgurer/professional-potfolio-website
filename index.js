cd676d5ca9ac8e27d9932ee9f877b269f6106f4f
Cameron McGurer <71100101+cammcgurer@users.noreply.github.com>
2024-05-11 09:27:29 -0400

    Move index.js to root

---
+++ index.js
@@ -0,0 +1,84 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <div className="navbar">
+          <div className="navbar-brand">
+              <strong>Cameron McGurer</strong>
+          </div>
+          <div className="navbar-links">
+            <a href="https://github.com/cammcgurer" target="_blank" aria-label="GitHub">
+              <img src="github.png" alt="GitHub" />
+            </a>
+            <a href="https://www.linkedin.com/in/cameronmcgurer/" target="_blank" aria-label="LinkedIn">
+             <img src="linkedin.png" alt="LinkedIn" />
+            </a>
+          </div>
+        </div>
+    
+        <div className="intro-section">
+          <div className="image-container">
+            <Image
+              src="/cam_headshot.png"
+              width={325}
+              height={325}
+              alt="Cameron"
+            />
+          </div>
+          <div className="intro-text">
+            <h1>Hello, I'm Cameron</h1>
+            <p>I am a young determined individual on the East Coast interested in computer science, business, and the overlap between the two.
+            </p>
+          </div>
+          <div className="intro-down-arrow">
+            <img src="arrow.png" alt="Down Arrow" />
+          </div>
+        </div>
+
+        <Header title="Projects" />
+        <div className="project-section">
+          <ul className="projects-list">
+            <li>
+              <h3>Business Applications of Generative AI</h3>
+              <p>A practicum involving the research and study of ways that generative artificial intelligence can be used inthe business world</p>
+            </li>
+            <li>
+              <h3>Universal Machine</h3>
+              <p>A software implementation of a virtual machine coded in Rust. This program takes, as input, a data file consisting of instruction
+                 words and performs 14 different operations based on the information in the instruction. It uses eight registers, a two-dimensional array
+                 of memory, and a program counter</p>
+            </li>
+            <li>
+              <h3>Travelers on a Map</h3>
+              <p>Uses multithreading to launch a certain number of threads, or travelers, on a two-dimensional grid. 
+                 This grid contains moving and stationary walls that the travelers cannot pass through, an exit that the travelers are trying to reach, and
+                 a lock for every square to prevent deadlocks.</p>
+            </li>
+          </ul>
+        </div>
+
+        <Header title="Resume" />
+        <div className="resume-section">
+          <iframe
+            src="/Cam_McGurer_Resume.pdf"
+            allow="fullscreen">
+          </iframe>
+        </div>
+                
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
+
